1f8be6453057dd242854ae474eccbbbca3715643
zenywallet <zenywallet@bitzeny.jp>
2022-08-25 06:29:11 +0900

    add caprese

---
+++ src/caprese.nim
@@ -0,0 +1,15 @@
+# Copyright (c) 2022 zenywallet
+
+import posix
+import server
+
+
+when isMainModule:
+  onSignal(SIGINT, SIGTERM):
+    echo "bye from signal ", sig
+    server.stop()
+
+  signal(SIGPIPE, SIG_IGN)
+
+  setUlimit(ULIMIT_SIZE)
+  start()
