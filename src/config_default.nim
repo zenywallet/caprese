c993b5488ec46b9951d986d786451189f8d35589
zenywallet <zenywallet@bitzeny.jp>
2022-08-28 00:35:18 +0900

    add config default

---
+++ src/config_default.nim
@@ -0,0 +1,6 @@
+const HTTP_PORT = 8089
+const HTTPS_PORT = 8009
+const HTTP_HOST_NAME = "localhost:8089"
+const HTTPS_HOST_NAME = "localhost:8009"
+const REDIRECT_URL = "http://" & HTTPS_HOST_NAME
+const DEBUG_LOG = true
