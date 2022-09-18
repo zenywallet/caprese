030f2a9f7141e740961c0ebc935b92051ecd5276
zenywallet <zenywallet@bitzeny.jp>
2022-09-18 17:48:05 +0900

    add ssl related default config

--- src/config_default.nim
+++ src/config_default.nim
@@ -4,3 +4,9 @@ const HTTP_HOST_NAME = "localhost:8089"
 const HTTPS_HOST_NAME = "localhost:8009"
 const REDIRECT_URL = "http://" & HTTPS_HOST_NAME
 const DEBUG_LOG = true
+
+const CERT_PATH = "."
+const CERT_FILE = CERT_PATH / "cert.pem"
+const PRIVKEY_FILE = CERT_PATH / "privkey.pem"
+const CHAIN_FILE = CERT_PATH / "fullchain.pem"
+const SSL_AUTO_RELOAD = true
