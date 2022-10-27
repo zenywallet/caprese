bddd82e9766f26063148533760194af1087a59da
zenywallet <zenywallet@bitzeny.jp>
2022-10-28 01:25:59 +0900

    add static html template

--- src/contents.nim
+++ src/contents.nim
@@ -1,6 +1,10 @@
 # Copyright (c) 2021 zenywallet
 
 import statuscode
+import macros
+import os
+import strutils
+export macros except error
 
 const HTTP_VERSION* = 1.1
 
@@ -47,3 +51,32 @@ const TooMany* = "<!DOCTYPE html><meta charset=\"utf-8\"><i>Take your time.</i>"
 const Empty* = ""
 
 template addDocType*(body: string): string = "<!DOCTYPE html><meta charset=\"utf-8\">" & body
+
+const srcFile = currentSourcePath()
+const (srcFileDir, srcFieName, srcFileExt) = splitFile(srcFile)
+
+var tmpFileId {.compileTime.}: int = 0
+
+template staticHtmlDocument*(body: untyped): string =
+  mixin unindent
+  var ret: string
+  block:
+    macro staticHtmlDocumentMacro(): string =
+      let randomStr = staticExec("head -c 100 /dev/urandom | tr -dc A-Za-z0-9 | head -c13")
+      inc(tmpFileId)
+      let id = $tmpFileId & "_" & randomStr
+      let tmpExeFile = srcFileDir / srcFieName & "_tmp" & $id
+      let tmpSrcFile = tmpExeFile & srcFileExt
+      var code = "import re\n" &
+        "let content = \"\"\"" & $body & "\"\"\"\n" &
+        """echo "<!DOCTYPE html>\n" & content.replacef(re"<([^>]*) />", "<$1>")""" & "\n"
+      writeFile(tmpSrcFile, code)
+      echo staticExec("nim c " & tmpSrcFile)
+      let content = staticExec(tmpExeFile).unindent(2)
+      discard staticExec("rm " & tmpExeFile)
+      discard staticExec("rm " & tmpSrcFile)
+      nnkStmtList.newTree(
+        newLit(content)
+      )
+    ret = staticHtmlDocumentMacro()
+  ret
