50b2aeb1c4c9040545eafcdc97c553e789637276
zenywallet <zenywallet@bitzeny.jp>
2022-11-19 09:26:55 +0900

    add periodically check if ssl files are watchable

--- src/server.nim
+++ src/server.nim
@@ -1391,6 +1391,8 @@ when ENABLE_SSL:
           setSslFilesWatch()
         else:
           return
+      else:
+        setSslFilesWatch()
       try:
         var changeFlag = false
         for i, site in CERT_SITES:
