0e0b4c09e62cb9470af653e923f6b35e0f031cfc
zenywallet <zenywallet@bitzeny.jp>
2023-01-12 11:03:09 +0900

    move HttpTargetHeader to serverdef

--- src/server.nim
+++ src/server.nim
@@ -1879,56 +1879,6 @@ proc setClientSocketLock(sock: cint, threadId: int): bool {.inline.} =
 proc resetClientSocketLock(threadId: int) {.inline.} =
   clientSocketLocks[threadId] = osInvalidSocket.cint
 
-macro HttpTargetHeader(idEnumName, valListName, targetHeaders, body: untyped): untyped =
-  var enumParams = nnkEnumTy.newTree(newEmptyNode())
-  var targetParams = nnkBracket.newTree()
-  var headers = nnkBracket.newTree()
-  for a in body:
-    enumParams.add(a[0])
-    var paramLit = newLit($a[1][0] & ": ")
-    targetParams.add(paramLit)
-    headers.add(nnkTupleConstr.newTree(
-      nnkExprColonExpr.newTree(
-        newIdentNode("id"),
-        a[0]
-      ),
-      nnkExprColonExpr.newTree(
-        newIdentNode("val"),
-        paramLit
-      )
-    ))
-
-  nnkStmtList.newTree(
-    nnkTypeSection.newTree(
-      nnkTypeDef.newTree(
-        idEnumName,
-        newEmptyNode(),
-        enumParams
-      )
-    ),
-    nnkConstSection.newTree(
-      nnkConstDef.newTree(
-        valListName,
-        newEmptyNode(),
-        targetParams
-      )
-    ),
-    nnkVarSection.newTree(
-      nnkIdentDefs.newTree(
-        targetHeaders,
-        newEmptyNode(),
-        nnkPrefix.newTree(
-          newIdentNode("@^"),
-          headers
-        )
-      )
-    )
-  )
-
-macro HttpTargetHeader*(body: untyped): untyped =
-  quote do:
-    HttpTargetHeader(HeaderParams, TargetHeaderParams, TargetHeaders, `body`)
-
 template serverType() {.dirty.} =
   type
     ReqHeader = object
--- src/serverdef.nim
+++ src/serverdef.nim
@@ -1,5 +1,7 @@
 # Copyright (c) 2022 zenywallet
 
+import macros
+
 type
   ClientId* = int
 
@@ -9,3 +11,54 @@ type
     Success = 1
     Pending = 2
     Invalid = 3
+
+
+macro HttpTargetHeader(idEnumName, valListName, targetHeaders, body: untyped): untyped =
+  var enumParams = nnkEnumTy.newTree(newEmptyNode())
+  var targetParams = nnkBracket.newTree()
+  var headers = nnkBracket.newTree()
+  for a in body:
+    enumParams.add(a[0])
+    var paramLit = newLit($a[1][0] & ": ")
+    targetParams.add(paramLit)
+    headers.add(nnkTupleConstr.newTree(
+      nnkExprColonExpr.newTree(
+        newIdentNode("id"),
+        a[0]
+      ),
+      nnkExprColonExpr.newTree(
+        newIdentNode("val"),
+        paramLit
+      )
+    ))
+
+  nnkStmtList.newTree(
+    nnkTypeSection.newTree(
+      nnkTypeDef.newTree(
+        idEnumName,
+        newEmptyNode(),
+        enumParams
+      )
+    ),
+    nnkConstSection.newTree(
+      nnkConstDef.newTree(
+        valListName,
+        newEmptyNode(),
+        targetParams
+      )
+    ),
+    nnkVarSection.newTree(
+      nnkIdentDefs.newTree(
+        targetHeaders,
+        newEmptyNode(),
+        nnkPrefix.newTree(
+          newIdentNode("@^"),
+          headers
+        )
+      )
+    )
+  )
+
+macro HttpTargetHeader*(body: untyped): untyped =
+  quote do:
+    HttpTargetHeader(HeaderParams, TargetHeaderParams, TargetHeaders, `body`)
