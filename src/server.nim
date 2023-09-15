e59f91c93d3563223464608799760a8a1a29b43b
zenywallet <zenywallet@bitzeny.jp>
2023-09-16 00:34:23 +0900

    change wsSend result

--- src/server.nim
+++ src/server.nim
@@ -859,16 +859,16 @@ template serverTagLib*(cfg: static Config) {.dirty.} =
                   opcode: WebSocketOpCode = WebSocketOpCode.Binary): SendResult {.dirty.} =
     clientId.wsServerSend(data, opcode)
 
-  proc wsSend(tag: Tag, data: seq[byte] | string | Array[byte],
-                opcode: WebSocketOpCode = WebSocketOpCode.Binary): SendResult =
+  proc wsSend0(tag: Tag, data: seq[byte] | string | Array[byte],
+              opcode: WebSocketOpCode = WebSocketOpCode.Binary): SendResult =
     result = SendResult.Success
     for cid in tag.getClientIds():
       let ret = cid.wsServerSend(data, opcode)
       if ret == SendResult.Pending:
         result = SendResult.Pending
 
-  proc wsSendTag(tag: Tag, data: seq[byte] | string | Array[byte],
-                opcode: WebSocketOpCode = WebSocketOpCode.Binary): int =
+  proc wsSend(tag: Tag, data: seq[byte] | string | Array[byte],
+              opcode: WebSocketOpCode = WebSocketOpCode.Binary): int =
     for cid in tag.getClientIds():
       let ret = cid.wsServerSend(data, opcode)
       if ret == SendResult.Pending or ret == SendResult.Success:
