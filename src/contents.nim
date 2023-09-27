e0a703f9648b2af1255182eab36ff6fae0efb2c8
zenywallet <zenywallet@bitzeny.jp>
2023-09-27 15:44:02 +0900

    add contents addHeader non-static mimetype

--- src/contents.nim
+++ src/contents.nim
@@ -109,6 +109,35 @@ proc addHeaderBrotli*(body: string, etag: string, code: static StatusCode = Stat
           "Content-Length: " & $body.len & "\c\L\c\L" &
           body
 
+proc addHeader*(body: string, etag: string, code: static StatusCode = Status200, mimetype: string = "text/html"): string =
+  result = "HTTP/" & $HTTP_VERSION & " " & $code & "\c\L" &
+          "Content-Type: " & mimetype & "\c\L" &
+          "ETag: " & etag & "\c\L" &
+          "Date: " & getCurTimeStr() & "\c\L" &
+          "Server: " & ServerName & "\c\L" &
+          "Content-Length: " & $body.len & "\c\L\c\L" &
+          body
+
+proc addHeaderDeflate*(body: string, etag: string, code: static StatusCode = Status200, mimetype: string = "text/html"): string =
+  result = "HTTP/" & $HTTP_VERSION & " " & $code & "\c\L" &
+          "Content-Type: " & mimetype & "\c\L" &
+          "ETag: " & etag & "\c\L" &
+          "Content-Encoding: deflate\c\L" &
+          "Date: " & getCurTimeStr() & "\c\L" &
+          "Server: " & ServerName & "\c\L" &
+          "Content-Length: " & $body.len & "\c\L\c\L" &
+          body
+
+proc addHeaderBrotli*(body: string, etag: string, code: static StatusCode = Status200, mimetype: string = "text/html"): string =
+  result = "HTTP/" & $HTTP_VERSION & " " & $code & "\c\L" &
+          "Content-Type: " & mimetype & "\c\L" &
+          "ETag: " & etag & "\c\L" &
+          "Content-Encoding: br\c\L" &
+          "Date: " & getCurTimeStr() & "\c\L" &
+          "Server: " & ServerName & "\c\L" &
+          "Content-Length: " & $body.len & "\c\L\c\L" &
+          body
+
 proc redirect301*(location: string): string =
   result = "HTTP/" & $HTTP_VERSION & " " & $Status301 & "\c\L" &
           "Content-Type: text/html\c\L" &
