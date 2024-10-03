a2eb2ae9d2f97872dc3a24f87f9c9a96325bb147
zenywallet <zenywallet@bitzeny.jp>
2024-10-04 00:44:30 +0900

    remove evalSslLib for cfg

--- src/caprese/server.nim
+++ src/caprese/server.nim
@@ -1266,18 +1266,9 @@ macro addServerMacro*(bindAddress: string, port: uint16, unix: bool, ssl: bool, 
     if retCtl != 0:
       errorRaise "error: addServer epoll_ctl ret=", retCtl, " ", getErrnoStr()
 
-
-macro evalSslLib(val: SslLib): SslLib =
-  quote do:
-    when `val` == BearSSL: BearSSL
-    elif `val` == OpenSSL: OpenSSL
-    elif `val` == LibreSSL: LibreSSL
-    elif `val` == BoringSSL: BoringSSL
-    else: None
-
 template addServer*(bindAddress: string, port: uint16, unix: bool, ssl: bool, body: untyped) =
   initServer()
-  addServerMacro(bindAddress, port, unix, ssl, evalSslLib(cfg.sslLib), body)
+  addServerMacro(bindAddress, port, unix, ssl, cfg.sslLib, body)
 
 macro serverConfigMacro*(): untyped = serverConfigStmt
 
