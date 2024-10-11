9bf89fffc3aba449addae1320c1fdc1021122df8
zenywallet <zenywallet@bitzeny.jp>
2024-10-12 00:46:22 +0900

    remove anonymous proc

--- src/caprese/server2.nim
+++ src/caprese/server2.nim
@@ -6,28 +6,27 @@ echo "welcome server2!"
 
 template parseServers*(serverBody: untyped) =
   macro parseBody() =
-    (proc () =
-      macro addServer(bindAddress: string, port: uint16, unix: bool, ssl: bool, body: untyped): untyped =
-        echo "server"
-        body
+    macro addServer(bindAddress: string, port: uint16, unix: bool, ssl: bool, body: untyped): untyped =
+      echo "server"
+      body
 
-      macro routes(routesBody: untyped): untyped =
-        var ret = newStmtList()
-        ret.add quote do:
-          echo "routes"
-        ret.add(routesBody)
-        ret
+    macro routes(routesBody: untyped): untyped =
+      var ret = newStmtList()
+      ret.add quote do:
+        echo "routes"
+      ret.add(routesBody)
+      ret
 
-      macro get(url: string, getBody: untyped): untyped =
-        quote do:
-          echo "get"
+    macro get(url: string, getBody: untyped): untyped =
+      quote do:
+        echo "get"
 
-      macro post(url: string, postBody: untyped): untyped =
-        quote do:
-          echo "post"
+    macro post(url: string, postBody: untyped): untyped =
+      quote do:
+        echo "post"
 
-      macro serverBodyMacro(): untyped =
-        serverBody
-      serverBodyMacro()
-    )()
+    macro serverBodyMacro(): untyped =
+      serverBody
+    serverBodyMacro()
+
   parseBody()
