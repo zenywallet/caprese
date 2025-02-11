0b52091d0b27b0d99e6d22e47edaf5d7efeae736
zenywallet <zenywallet@bitzeny.jp>
2025-02-11 23:41:49 +0900

    add bearssl block import comment

--- src/caprese/bearssl/block.nim
+++ src/caprese/bearssl/block.nim
@@ -1,2 +1,9 @@
 import bearssl_block
 export bearssl_block
+
+# block is a reserved word, try the next one
+#import bearssl/"block"
+#import "bearssl/block"
+#import bearssl.block
+#import bearssl/bearssl_block
+#import bearssl.bearssl_block
