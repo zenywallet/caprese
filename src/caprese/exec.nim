d9e634818330245064c433caa8a17e7ac477cf77
zenywallet <zenywallet@bitzeny.jp>
2025-10-20 00:07:35 +0900

    change exec cache dir

--- src/caprese/exec.nim
+++ src/caprese/exec.nim
@@ -8,7 +8,6 @@ import strutils
 const srcFile = currentSourcePath()
 const (srcFileDir, srcFileName, srcFileExt) = splitFile(srcFile)
 const binDir = srcFileDir / ".." / "bin"
-const cacheDir = srcFileDir / "nimcache"
 const execHelperExe = binDir / "exec_helper"
 const execHelperSrc = srcFileDir / "exec_helper" & srcFileExt
 
@@ -43,6 +42,7 @@ proc execCode*(srcFileDir: string, code: string, rstr: string): string {.compile
   let exeFileName = srcFileName & "_tmp" & $tmpFileId & rstr
   let tmpExeFile = srcFileDir / exeFileName
   let tmpSrcFile = tmpExeFile & srcFileExt
+  let cacheDir = srcFileDir / "nimcache"
   let tmpCacheDir = cacheDir / exeFileName
   writeFile(tmpSrcFile, code)
   echo staticExec("nim c --nimcache:" & tmpCacheDir & " " & tmpSrcFile)
