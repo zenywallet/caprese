fd423eb5ba317687b150b56ee340411b31c202be
zenywallet <zenywallet@bitzeny.jp>
2026-03-05 01:55:29 +0900

    change keepIndent

--- src/caprese/contents.nim
+++ src/caprese/contents.nim
@@ -294,7 +294,7 @@ template convertHtmlDocument*(srcFileDir, code: string): string =
   echo ret
   ret
 
-proc keepIndent*(code: string): string = ("\n" & code).indent(4)
+proc keepIndent*(code: string): string = "\n" & code.indent(4)
 
 when NimVersion == "2.0.4": # workaround modulepaths.nim/getModuleName degrade
   import karax.karaxdsl
